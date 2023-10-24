44eabd91706879942e1142b2209b51f242b31210
Jameel Alsalam <alsalam.jameel@epa.gov>
2023-10-24 15:15:24 -0400

    move some packages to suggest

--- R/get.R
+++ R/get.R
@@ -1,6 +1,9 @@
 
 #' Get values of an idx_tbl for specific index values
 #'
+#' @param x an idx_tbl
+#' @param idx an index that can be used with x
+#'
 #' @export
 value_at_idx <- function(x, idx) {
 
